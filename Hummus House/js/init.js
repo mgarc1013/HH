1120451d0e00a01bd3e20f5cb70dc2d329d484a4
mgarc1013 <35808821+mgarc1013@users.noreply.github.com>
2019-03-12 11:57:55 -0400

    Add files via upload

---
+++ Hummus House/js/init.js
@@ -0,0 +1,16 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+$(document).ready(function() {
+  $('select').material_select();
+});
+      
+
+
+
+
